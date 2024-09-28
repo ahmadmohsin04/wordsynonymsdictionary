fe9e0ee5a7d217f6fbfe51dd7e184cadebb259ed
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-09-28 18:47:03 +0500

    inserting

--- src/SynonymDictionary.java
+++ src/SynonymDictionary.java
@@ -1,5 +1,23 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SynonymDictionary {
+
+    private HashMap<String, List<String>> synonymMap;
+
+    public SynonymDictionary() {
+        synonymMap = new HashMap<>();
+    }
+
+    public void insertWord(String word, List<String> synonyms) {
+        if (synonymMap.containsKey(word)) {
+            synonymMap.get(word).addAll(synonyms);
+        } else {
+            synonymMap.put(word, new ArrayList<>(synonyms));
+        }
+        System.out.println("Word \"" + word + "\" added or updated successfully");
+    }
+    public static void main(String[] args)  {
     }
 }
